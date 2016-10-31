084176ad50d2cce49a18a235eac6076040257022
rajesh <rajesheluru@yahoo.com>
2016-10-31 00:16:53 -0400

    portpolio first commit

---
+++ sendmail.php
@@ -0,0 +1,12 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $name = $_POST['name'];
+    $to = "rajesheluru@yahoo.com";
+    $subject = "New message";
+
+    mail ($to, $subject, $message, "From: " . $name);
+    echo "Your message has been sent.";
+?>
